933fa27ab08df00bc6201a01925812d1360a208e
DavidMukhiya <sherlock.41v94@gmail.com>
2022-04-16 18:57:12 +0545

    Creating simple magic 8 ball

---
+++ android/app/src/main/kotlin/com/davidmukhiya/magic_8_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.davidmukhiya.magic_8_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Ask Me Anything"),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Image.asset("images/ball1.png")),
+    );
+  }
+}
